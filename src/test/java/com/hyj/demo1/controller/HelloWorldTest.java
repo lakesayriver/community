8c31fac024e103c8808fb90fe8e225b162834f60
lakesayriver <1172497454@qq.com>
2019-08-30 20:09:15 +0800

    frist commit

---
+++ src/test/java/com/hyj/demo1/controller/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.hyj.demo1.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    public void testHelloWorld()
+    {
+
+    }
+}
